e2354a17b4e59b1b4ce39bfcfc90bd47a5c0168a
Leslie Raganit <niki@MacBook-Pro-6.local>
2023-06-20 00:03:56 -0700

    reduce list based off of incorrectly placed letters "yellow"

--- main.js
+++ main.js
@@ -17,50 +17,50 @@ parsedData = JSON.parse(data)
 //     return word
 // }
 
-// function letterColors(wordList) {
-//     const colorArr =[]
-//     const word = randomWordGenerator(wordList)
+const colorArr =[]
 
-//     function createColorArr(word) {
-        
-//         const letters = word.split("")
-//         const question = (colorArr.length < 5) ? `Can you please tell me the color of the letter "${letters[colorArr.length]}"? ` : "Thank you so much for your help! "
-//         rl.question(question, (answer) => {
-//             if (colorArr.length == 5) {
-//                 rl.close();
-//                 return colorArr;
-//             }
+function createColorArr(word) {
+    const letters = word.split("")
+    const question = (colorArr.length < 5) ? `Can you please tell me the color of the letter "${letters[colorArr.length]}"? ` : "Thank you so much for your help! "
+    rl.question(question, (answer) => {
+        if (colorArr.length == 5) {
+            rl.close();
+            return colorArr;
+        }
 
-//             else {
-//             colorArr.push(answer.toLowerCase())
-//             createColorArr(word)
-//             }
+        else {
+        colorArr.push(answer.toLowerCase())
+        createColorArr(word)
+        }
 
-//     })}
-//     createColorArr(word)
-// }
+})}
 
-const shortWordList = ["green", "gravy", "grace", "words", "extra", "tapes"]
+
+const shortWordList = ["green", "gravy", "grace", "words", "extra", "tapes", "great"]
 
 function reduceWordList (wordList) {
     var changedWordList = wordList
-    const colorList = ["green", "green", "green", "green", "grey"]
+    const colorList = ["green", "green", "green", "green", "green"]
     const letters = "great".split("")
     console.log(letters)
 
     for (i in colorList) {
-        if (colorList[i]=="green") {
+        console.log(letters[i])
+        if (colorList[i] =="green") {
             const filteredListGreen = changedWordList.filter(word => word.split("")[i] == letters[i])
             changedWordList = filteredListGreen
-            console.log("green", filteredListGreen)
-            console.log(changedWordList)
+            console.log("green", changedWordList)
 
     }
-            // if (colorList[i]=="yellow") {
-        //     const filteredListYellow = 
-        // }
+        if (colorList[i]=="yellow") {
+            const filteredListYellow = changedWordList.filter(word => { 
+                return word.split("")[i] != letters[i] && word.includes(letters[i])
+            })
+            changedWordList = filteredListYellow
+            console.log('yellow', changedWordList)
+        }
     else {
-        console.log("I'm not green")
+        console.log("I'm grey")
     }
 }
 }
