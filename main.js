2c46b96c6bda1832470ca1616c0592730c3fb149
Leslie Raganit <niki@MacBook-Pro-6.local>
2023-06-21 14:23:40 -0700

    working wordle solver!

--- main.js
+++ main.js
@@ -5,64 +5,97 @@ const data = fs.readFileSync('words.json',
     { encoding: 'utf8', flag: 'r' });
 parsedData = JSON.parse(data)
 
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// })
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
-// function randomWordGenerator(wordList) {
-//     const wordListLength = wordList.length
-//     const word = wordList[Math.floor(Math.random() * wordListLength)]
-//     console.log(word)
-//     return word
-// }
+main(parsedData)
 
-const colorArr =[]
+function main(wordList) {
+    var mainWordList = wordList;
 
-function createColorArr(word) {
-    const letters = word.split("")
-    const question = (colorArr.length < 5) ? `Can you please tell me the color of the letter "${letters[colorArr.length]}"? ` : "Thank you so much for your help! "
-    rl.question(question, (answer) => {
-        if (colorArr.length == 5) {
-            rl.close();
-            return colorArr;
+    function processWord(count) {
+        console.log(mainWordList.length)
+        var colorArr = []
+        if (count < 6) {
+            var randomword = randomWordGenerator(mainWordList);
+            createColorArr(randomword, colorArr, (result) => {
+                mainWordList = reduceWordList(randomword, mainWordList, result);
+
+                processWord(count + 1);
+            })
+        }
+        
+        else if (colorArr.every(color => color == "green")) {
+            return ("Congrats on getting the correct word!")
+        }
+
+        else if (count == 6) {
+            return ("You have reached the retry limit :( ")
         }
 
         else {
-        colorArr.push(answer.toLowerCase())
-        createColorArr(word)
+            rl.close();
+        }
+    }
+    processWord(0)
+}
+
+
+
+function randomWordGenerator(wordList) {
+    const wordListLength = wordList.length
+    const word = wordList[Math.floor(Math.random() * wordListLength)]
+    console.log(word)
+    return word
+}
+
+
+function createColorArr(word, colorArr, callback) {
+    const colorArrMaker = colorArr
+    // console.log('colorArr', colorArrMaker.length, colorArrMaker)
+    const letters = word.split("")
+    const question = (colorArrMaker.length < 5) ? `Can you please tell me the color of the letter "${letters[colorArrMaker.length]}"? ` : `Thank you so much for your help! ${colorArrMaker}`
+    rl.question(question, (answer) => {
+        if (colorArrMaker.length == 5) {
+
+            callback(colorArrMaker);
         }
 
-})}
+        else {
+        colorArrMaker.push(answer.toLowerCase())
+        createColorArr(word, colorArr, callback)
+        }
+}
+)
+}
 
-
-const shortWordList = ["green", "gravy", "grace", "words", "extra", "tapes", "great"]
-
-function reduceWordList (wordList) {
-    var changedWordList = wordList
-    const colorList = ["green", "green", "green", "green", "green"]
-    const letters = "great".split("")
-    console.log(letters)
+function reduceWordList (word, wordList, colorArr) {
+    var filteredList = wordList
+    const colorList = colorArr
+    const letters = word.split("")
 
     for (i in colorList) {
-        console.log(letters[i])
         if (colorList[i] =="green") {
-            const filteredListGreen = changedWordList.filter(word => word.split("")[i] == letters[i])
-            changedWordList = filteredListGreen
-            console.log("green", changedWordList)
+            const filteredListGreen = filteredList.filter(word => word.split("")[i] == letters[i])
+            filteredList = filteredListGreen
+            // console.log("green", filteredList)
 
     }
         if (colorList[i]=="yellow") {
-            const filteredListYellow = changedWordList.filter(word => { 
+            const filteredListYellow = filteredList.filter(word => { 
                 return word.split("")[i] != letters[i] && word.includes(letters[i])
             })
-            changedWordList = filteredListYellow
-            console.log('yellow', changedWordList)
+            filteredList = filteredListYellow
+            // console.log('yellow', filteredList)
         }
-    else {
-        console.log("I'm grey")
+        if (colorList[i] =="gray") {
+            const filteredListGray = filteredList.filter(word => !word.includes(letters[i]))
+            filteredList = filteredListGray
+            // console.log('gray', filteredList)
+        }   
     }
-}
+    return filteredList
 }
 
-console.log(reduceWordList(shortWordList))
