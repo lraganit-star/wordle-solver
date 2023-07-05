4a66cc5d6306dcb851fba9bae8143e0ed83340d4
Leslie Raganit <niki@MacBook-Pro-6.local>
2023-07-05 16:47:44 -0700

    wordle solver with word frequency optimization

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ const readline = require('readline')
 const _ = require('lodash');
 const neatCsv = require('neat-csv')
 
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// })
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
 const wordJSON = fs.readFileSync('words.json',
     { encoding: 'utf8', flag: 'r' });
@@ -21,11 +21,11 @@ async function frequencyArr() {
         const parsedData = await neatCsv(data);
         
         let dataMap = {}; 
-        parsedData.forEach(item => dataMap[item.word] = item.count);
+        parsedData.forEach(item => dataMap[item.word] = parseInt(item.count));
 
         const union = wordleArr.map(word => dataMap[word] 
             ? {word, count: dataMap[word]}
-            : {word: word, count: 0}
+            : {word: word, count: parseInt('0')}
         );
         return union
         }
@@ -37,7 +37,7 @@ async function frequencyArr() {
 
 (async () => {
     try {
-        const mainWordList = await frequencyArr();
+        var mainWordList = await frequencyArr();
 
         function processWord(count) {
             var colorArr = []
@@ -45,17 +45,19 @@ async function frequencyArr() {
                 var bestWord = mostFrequentWord(mainWordList).word;
                 console.log('bestWord', bestWord)
                 createColorArr(bestWord, colorArr, (result) => {
-                    mainWordList = reduceWordList(randomword, mainWordList, result);
+                    mainWordList = reduceWordList(bestWord, mainWordList, result);
     
                     processWord(count + 1);
                 })
             }
             
             else if (colorArr.every(color => color == "green")) {
+                rl.close();
                 return ("Congrats on getting the correct word!")
             }
     
             else if (count == 6) {
+                rl.close();
                 return ("You have reached the retry limit :( ")
             }
     
@@ -99,28 +101,45 @@ function createColorArr(word, colorArr, callback) {
 }
 
 function reduceWordList (bestWord, wordList, colorArr) {
-    var filteredList = wordList
+    var justWordsArr = [];
+    for (let i in wordList) {
+        justWordsArr.push(wordList[i].word)
+    }
+
     const colorList = colorArr
     const letters = bestWord.split("")
-
+    var justWords = justWordsArr
+    
     for (i in colorList) {
+        console.log(justWords.length)
         if (colorList[i] =="green") {
-            const filteredListGreen = filteredList.filter(word => word.split("")[i] == letters[i])
-            filteredList = filteredListGreen
+            const filteredListGreen = justWords.filter(word => word.split("")[i] == letters[i])
+            justWords = filteredListGreen
         }
 
+        // probably have to edit yellow to account for double letters
         if (colorList[i]=="yellow") {
-            const filteredListYellow = filteredList.filter(word => { 
+            const filteredListYellow = justWords.filter(word => { 
                 return word.split("")[i] != letters[i] && word.includes(letters[i])
             })
-            filteredList = filteredListYellow
+            justWords = filteredListYellow
         }
 
         if (colorList[i] == "gray" || colorList[i] == "grey" ) {
-            const filteredListGray = filteredList.filter(word => word.split("")[i] != letters[i])
-            filteredList = filteredListGray
+            const filteredListGray = justWords.filter(word => !word.includes(letters[i]))
+            justWords = filteredListGray
         }   
     }
+
+    console.log(justWords)
+    var filteredList = []
+    for (i in wordList) {
+        if (justWords.includes(wordList[i].word)) {
+            filteredList.push(wordList[i])
+        }
+    
+    }
+    console.log(filteredList)
     return filteredList
 }
 
