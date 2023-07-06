54381adf87ca1e9f572293273fee787b982dd8af
Leslie Raganit <niki@MacBook-Pro-6.local>
2023-07-06 13:41:15 -0700

    added react for UI

---
+++ wordle_solver/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import "./App.css";
+
+// function Board() {
+//   return (
+//     <>
+//       <div id="board">
+//         <div id="firstWord"></div>
+//         <div id="secondWord"></div>
+//         <div id="thirdWord"></div>
+//         <div id="fourthWord"></div>
+//         <div id="fifthWord"></div>
+//         <div id="sixthWord"></div>
+//       </div>
+//     </>
+//   );
+// }
+
+// function Word() {
+//   return (
+//     <>
+//       <div id="firstletter"></div>
+//       <div id="secondletter"></div>
+//       <div id="thirdletter"></div>
+//       <div id="fourthletter"></div>
+//       <div id="fifthletter"></div>
+//     </>
+//   );
+// }
+
+function Letter() {
+  return (
+    <>
+      <div id="letter"></div>
+    </>
+  );
+}
+
+export default Letter;
