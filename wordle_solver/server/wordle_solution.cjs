92a79100ec28200c3222791ca7b11a929d6ea181
Leslie Raganit <lnraganit@gmail.com>
2023-09-18 14:27:49 -0700

    added express and nodemon to server

---
+++ wordle_solver/server/wordle_solution.cjs
@@ -0,0 +1,149 @@
+const fs = require('fs');
+const fsPromise = require('fs').promises;
+const readline = require('readline')
+const _ = require('lodash');
+const neatCsv = require('neat-csv')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const wordJSON = fs.readFileSync('words.json',
+    { encoding: 'utf8', flag: 'r' });
+const wordleArr = JSON.parse(wordJSON)
+const csvFilePath = 'unigram_freq.csv';
+
+async function frequencyArr() {
+    try {
+        const data = await fsPromise.readFile(csvFilePath)
+        const parsedData = await neatCsv(data);
+        
+        let dataMap = {}; 
+        parsedData.forEach(item => dataMap[item.word] = parseInt(item.count));
+
+        const union = wordleArr.map(word => dataMap[word] 
+            ? {word, count: dataMap[word]}
+            : {word: word, count: parseInt('0')}
+        );
+        return union
+        }
+
+    catch (error) {
+        console.error('File reading not so happy right now')
+    }
+}
+
+(async () => {
+    try {
+        var mainWordList = await frequencyArr();
+
+        function processWord(count) {
+            // instead of createColorArr, need to be able to use colorArr created in App.jsx
+            var colorArr = []
+            if (count < 6) {
+                var bestWord = mostFrequentWord(mainWordList).word;
+                console.log('Your word is: ', bestWord)
+                // createColorArr(bestWord, colorArr, (result) => {
+                //     mainWordList = reduceWordList(bestWord, mainWordList, result);
+    
+                //     processWord(count + 1);
+                // })
+            }
+            
+            else if (colorArr.every(color => color == "green")) {
+                rl.close();
+                return ("Congrats on getting the correct word!")
+            }
+    
+            else if (count == 6) {
+                rl.close();
+                return ("You have reached the retry limit :( ")
+            }
+    
+            else {
+                rl.close();
+            }
+        }
+        processWord(0)
+    }
+    catch (error){
+        console.error('Error:', error)
+    }
+})();
+
+function mostFrequentWord(wordList) {
+    let maxi = {word: '', count: 0}
+    for ( let i in wordList ) {
+        if (wordList[i].count > maxi.count) {
+            maxi = wordList[i]
+        }
+    }
+    return maxi
+}
+
+
+// instead of using this function, should now make a new function 
+// to import colorArr from App.jsx
+
+// function createColorArr(word, colorArr, callback) {
+//     const colorArrMaker = colorArr
+//     const letters = word.split("")
+//     const question = (colorArrMaker.length < 5) ? `Can you please tell me the color of the letter "${letters[colorArrMaker.length]}"? ` : `Thank you so much for your help! ${colorArrMaker}`
+//     rl.question(question, (answer) => {
+//         if (colorArrMaker.length == 5) {
+
+//             callback(colorArrMaker);
+//         }
+
+//         else {
+//         colorArrMaker.push(answer.toLowerCase())
+//         createColorArr(word, colorArr, callback)
+//         }
+//     }
+//     )
+// }
+
+function reduceWordList (bestWord, wordList, colorArr) {
+    var justWordsArr = [];
+    for (let i in wordList) {
+        justWordsArr.push(wordList[i].word)
+    }
+
+    const colorList = colorArr
+    const letters = bestWord.split("")
+    var justWords = justWordsArr
+    
+    for (i in colorList) {
+        console.log(justWords.length)
+        if (colorList[i] =="green") {
+            const filteredListGreen = justWords.filter(word => word.split("")[i] == letters[i])
+            justWords = filteredListGreen
+        }
+
+        // probably have to edit yellow to account for double letters
+        if (colorList[i]=="yellow") {
+            const filteredListYellow = justWords.filter(word => { 
+                return word.split("")[i] != letters[i] && word.includes(letters[i])
+            })
+            justWords = filteredListYellow
+        }
+
+        if (colorList[i] == "gray" || colorList[i] == "grey" ) {
+            const filteredListGray = justWords.filter(word => !word.includes(letters[i]))
+            justWords = filteredListGray
+        }   
+    }
+
+    console.log(justWords)
+    var filteredList = []
+    for (i in wordList) {
+        if (justWords.includes(wordList[i].word)) {
+            filteredList.push(wordList[i])
+        }
+    
+    }
+    console.log(filteredList)
+    return filteredList
+}
+
